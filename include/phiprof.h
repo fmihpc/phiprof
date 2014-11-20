02c6401c45d50f655749ecc0c1feba350fab71bf
Ilja Honkonen <ilja.honkonen@fmi.fi>
2014-11-19 21:44:50 -0500

    Fix license in a few files.
    Should be LGPL not GPL.

--- include/phiprof.h
+++ include/phiprof.h
@@ -3,17 +3,18 @@ This file is part of the phiprof library
 
 Copyright 2010, 2011, 2012 Finnish Meteorological Institute
 
-Vlasiator is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3
-as published by the Free Software Foundation.
+Phiprof is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-Vlasiator is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef PHIPROF_H

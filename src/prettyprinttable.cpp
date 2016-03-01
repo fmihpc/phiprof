89dd81ac2099fbf2a916716c34af3842845db8a2
Sebastian von Alfthan <sebastian.von.alfthan@csc.fi>
2016-03-01 22:48:41 +0200

    Added support for merged cells.

--- src/prettyprinttable.cpp
+++ src/prettyprinttable.cpp
@@ -1,3 +1,4 @@
+#include <utility>      // std::pair, std::make_pair
 #include <iostream>    
 #include <sstream>    
 #include <iomanip>    
@@ -6,37 +7,42 @@
 #include <algorithm>
 #include "prettyprinttable.hpp"
 
-void PrettyPrintTable::addTitle(std::string& newTitle)
-{
+void PrettyPrintTable::addTitle(std::string& newTitle){
    title = newTitle;
 }
 
 
-void PrettyPrintTable::addElement(std::string element, uint indentLevel ){
+void PrettyPrintTable::addElement(std::string element, uint span, uint indentLevel ){
    std::stringstream buffer;
    buffer << std::left;
    for(int i = 0; i < _indentWidth * indentLevel; i++)
-      buffer <<" ";
+      buffer << " ";
    buffer << element;
-   table.back().push_back(buffer.str());
+   if(table.size() == 0) {
+      addRow();
+   }
+   table.back().push_back(std::make_pair(buffer.str(), span));
 }
 
-void PrettyPrintTable::addElement(int element){
+void PrettyPrintTable::addElement(int element, uint span){
    std::stringstream buffer;
-   // buffer <<  std::setw(_intWidth);
    buffer << element;
-   table.back().push_back(buffer.str());
+   if(table.size() == 0) {
+      addRow();
+   }
+   table.back().push_back(std::make_pair(buffer.str(), span));
 }
 
-void PrettyPrintTable::addElement(double element){
+void PrettyPrintTable::addElement(double element, uint span){
    std::stringstream buffer;
    //make sure we use default floats, and not fixed or other format
    buffer << std::setiosflags( std::ios::floatfield );
-   //set float precision, 6 is needed for ".", "e+xx" and a space
-   buffer << std::setprecision(_floatWidth-6); 
-//   buffer << std::setw(_floatWidth);
+   buffer << std::setprecision(_floatPrecision); 
    buffer << element;
-   table.back().push_back(buffer.str());
+   if(table.size() == 0) {
+      addRow();
+   }
+   table.back().push_back(std::make_pair(buffer.str(), span));
 }
 
 
@@ -50,60 +56,98 @@ void PrettyPrintTable::addHorizontalLine(){
 
 
 void PrettyPrintTable::addRow(){
-   table.push_back(std::vector<std::string>());
+   table.push_back(std::vector<std::pair<std::string, uint> >());
 }
 
+
 void PrettyPrintTable::print(std::ofstream& output, std::string& delimeter){
-   int nColumns;
-   std::vector<int> columnWidths;
+   uint nColumns;
+   std::vector<uint> columnWidths;
    
-   nColumns=0;   
-   for(int i=0; i <  table.size();i++){
-      nColumns = std::max(nColumns, (int) (table[i].size()));
-   }
-   columnWidths.resize(nColumns);
-   for(int i = 0; i <  table.size(); i++){
-      for(int j = 0; j <  table[i].size(); j++){
-         columnWidths[j] = std::max(columnWidths[j], (int)(table[i][j].length()) );
+   //How many columns
+   nColumns = 0;   
+   for(auto &row: table){
+      uint rowColumns = 0;
+      for(auto &cell: row){
+         rowColumns +=  cell.second;
       }
+      nColumns = std::max(nColumns, rowColumns);
+   }
+
+   //get width of each column. Spanned cells are divided over its
+   //columns equally (might not always give the tightest fit, TODO)
+   columnWidths.resize(nColumns);
+   for(auto &row: table){
+      uint countedColumns = 0;
+      for(int j = 0; j <  row.size(); j++){
+         uint span = row[j].second;
+         for(uint col = countedColumns; col  < countedColumns + span; col ++ ){
+            uint columnWidth = (row[j].first.length() + (span - 1) * delimeter.length()) / span;
+            columnWidths[col] = std::max(columnWidths[col], columnWidth);
+         }
+         countedColumns += span;
+      }
+
       
    }
-   int totWidth = 0;
-   for(int j = 0; j <  nColumns; j++){
-      totWidth += columnWidths[j] + delimeter.length();
+   
+   //get total width of the table
+   uint totWidth = delimeter.length();
+   for(auto &width: columnWidths) {
+      totWidth += width + delimeter.length();
    }
-   
-   
-   output << std::right;
 
+   //Output title
    if(title.length() > 0) {
+      output << std::right;
       output << "\n";
       output << std::setw( (totWidth+ title.length())/2);
       output << title;
       output << "\n";
       
    }
+   //Output the table
    output << std::left;
-   for(int i = 0; i <  table.size(); i++){
-      if(table[i].size() == 0){
+   for(auto &row: table){
+      if(row.size() == 0){
+         //empty row => print line
          output << std::setw(1);
          for (int j = 0; j < totWidth; j++) {
             output << "-";
          }
       }
       else{
-         
-         for(int j = 0; j < nColumns; j++){
-            if( j < table[i].size() )
-               output << std::setw( columnWidths[j] ) << table[i][j];
-            else
-               output << std::setw( columnWidths[j] ) << "";
+         output << delimeter;
+         uint printedColumns = 0;
+         for(int j = 0; j < row.size(); j++){
+            uint span = row[j].second;
+            uint width = 0;
+            for(uint col = printedColumns; col < printedColumns + span; col++) {
+               width += columnWidths[col];
+            }
+            width += (span - 1) * delimeter.length();
+            if( span > 1) {
+               //print merged cells as centered
+               uint indent = (width - row[j].first.length())/2;
+               for(uint ind = 0; ind < indent; ind++){
+                  output << std::setw(1) << " ";
+               }
+               output << std::setw(width - indent) << row[j].first;
+            }
+            else {
+               output << std::setw(width) << row[j].first;
+            }
+            
+            output << delimeter;
+            printedColumns += span;
+         }
+
+         for(int j = printedColumns; j < nColumns; j++){
+            output << std::setw( columnWidths[j] ) << "";
             output << delimeter;
          }
       }
-      
       output<< "\n";
-      
    }
    
 }
--- src/prettyprinttable.hpp
+++ src/prettyprinttable.hpp
@@ -8,22 +8,19 @@ class PrettyPrintTable {
 public:
    void addHorizontalLine();
    void addTitle(std::string& title);
-   void addElement(std::string element, uint indentLevel = 0);
-   void addElement(int element);
-   void addElement(double element);
+   void addElement(std::string element, uint span = 1, uint indentLevel = 0);
+   void addElement(int element, uint span = 1);
+   void addElement(double element, uint span = 1);
    void addRow();
    void print(std::ofstream& file, std::string& delimeter);
    
 
 private:
    const int _indentWidth=2; //how many spaces each level is indented
-   const int _floatWidth=11; //width of float fields;
-   const int _intWidth=6;   //width of int fields;
-   const int _unitWidth=4;  //width of workunit label
-   const int _levelWidth=5; //width of level label
+   const int _floatPrecision = 5; //precision of float fields;
    
    std::string title;
-   std::vector<std::vector<std::string>> table;
+   std::vector<std::vector<std::pair<std::string, uint> > > table;
 };
 
    

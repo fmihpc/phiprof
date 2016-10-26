1131c57d13d3af8c91f08d1f9a3f17b45f80f132
Sebastian von Alfthan <sebastian.von.alfthan@csc.fi>
2016-10-26 14:37:33 +0300

    Added missing include for strtok

--- src/paralleltimertree.cpp
+++ src/paralleltimertree.cpp
@@ -23,6 +23,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #include <iomanip>    
 #include <vector>
 #include <string>
+#include <cstring>
 #include <set>
 #include <limits>
 #include <algorithm>
